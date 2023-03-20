74a3bb5132b82b8f3444c20af5f8bba5994222bc
ITStudent <ivanu1706@gmail.com>
2023-03-20 18:06:12 +1100

    а

--- Bot2/gpt3_5.py
+++ Bot2/gpt3_5.py
@@ -1,10 +1,13 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
+messages = []
+role_gpt = input('Chat GPT info about himself: ')
 
 while True:
-    promt = input()
-    messages = [{"role": "user", "content": promt}]
+    promt = input('Your promt: ')
+    messages.append({"role": "assistant", 'content' : role_gpt})
+    messages.append({"role": "user", 'content': promt})
 
     x = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -12,6 +15,6 @@ while True:
     )
 
     answer = x.choices[0].message.content
-    messages.append({"role": "system", 'content': answer})
+    print(f'ChatGPT: {answer}')
+    messages.append({"role": "assistant", 'content': answer})
 
-    print(answer)
